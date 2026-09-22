680d7c6bdb64d0d951303c88bc81940920d3ba5e
Mohammad Faysal <162318772+mohammad-faysal-dev@users.noreply.github.com>
2026-09-23 00:38:17 +0600

    feat: add role based sidebar routes

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -10,8 +10,10 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-
-
+import { Roles } from "@/constant/roles";
+import { adminRoutes } from "@/routes/admin.route";
+import { customerRoute } from "@/routes/customer.route";
+import { providerRoutes } from "@/routes/provider.route";
 
 type AppSidebarProps = {
   user: {
@@ -20,23 +22,28 @@ type AppSidebarProps = {
 };
 
 export function AppSidebar({ user }: AppSidebarProps) {
-  const routes = user.role === "admin" ? adminRoutes : userRoutes;
+  const routes =
+    user.role === Roles.admin
+      ? adminRoutes
+      : user.role === Roles.customer
+        ? customerRoute
+        : user.role === Roles.provider
+          ? providerRoutes
+          : [];
 
   return (
     <Sidebar>
       <SidebarContent>
         {routes.map((route) => (
           <SidebarGroup key={route.title}>
-            <h3 className="px-2 py-2 text-sm font-semibold">
-              {route.title}
-            </h3>
+            <h3 className="px-2 py-2 text-sm font-semibold">{route.title}</h3>
 
             <SidebarMenu>
               {route.items.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton >
+                  <SidebarMenuButton>
                     <Link href={item.url}>
-                      <item.icon className="size-4" />
+                      {/* <item.icon className="size-4" /> */}
                       <span>{item.title}</span>
                     </Link>
                   </SidebarMenuButton>
